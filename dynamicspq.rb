25f6d236cf4132cce742ae6942b12424299e89c5
jawbroken <jawbroken@gmail.com>
2009-05-14 11:45:52 +1000

    Dynamic SPQ with mouse interface prototyped.

--- dynamicspq.rb
+++ dynamicspq.rb
@@ -1,4 +1,6 @@
 require 'thread'
+require 'circle_aabb'
+
 Quadtree = Struct.new(:value, :topleft, :topright, :bottomleft, :bottomright)
 
 $quadtree = Quadtree.new(1, nil, nil, nil, nil)
@@ -7,11 +9,61 @@ $qmutex = Mutex.new
 $cursor_centre = [256, 256]
 $cursor_radius = 16
 
+def in_circle(x, y, centre, radius)
+  (x-centre[0])**2 + (y-centre[1])**2 <= radius**2
+end
+
+def leaf_node(qt)
+  !qt.value.nil?
+end
+
 def destroy_quadtree(qt, centre, radius)
-  def destroy_quadtree_r(qt, x, y, w, h, centre, radius)
-    
+  def destroy_quadtree_r(qt, x, y, w, centre, radius)
+    # Cases: Not overlapping - do nothing
+    #        Fully overlapping - delete all subtrees, replace with leaf
+    #        Partial overlap - subdivide and recur
+    #        Can replace full and partial overlap cases by merging if all
+    #        children are leaves. Inefficient though as it subdivides to 
+    #        1 pixel. Don't do this in a real implementation.
+    if w == 1
+      if in_circle(x, w, centre, radius)
+        Quadtree.new(0, nil, nil, nil, nil)
+      else
+        Quadtree.new(1, nil, nil, nil, nil)
+      end
+    end
+    if in_circle(x, y, centre, radius) and in_circle(x, y+w, centre, radius) and in_circle(x+w, y, centre, radius) and in_circle(x+w, y+w, centre, radius)
+      #full overlap
+      return Quadtree.new(0, nil, nil, nil, nil)
+    elsif circle_aabb_overlap?(Circle.new(Point.new(centre[0], centre[1]), radius),
+      AABB.new(Point.new(x, y), w))
+      #partial overlap
+      #subdivide leaf nodes and recur
+      if leaf_node(qt)
+        oldval = qt.value
+        qt = Quadtree.new(nil, Quadtree.new(oldval,nil,nil,nil,nil),
+          Quadtree.new(oldval,nil,nil,nil,nil), Quadtree.new(oldval,nil,nil,nil,nil),
+          Quadtree.new(oldval,nil,nil,nil,nil))
+      end
+      tlchild = destroy_quadtree_r(qt.topleft,x,y,w/2,centre,radius)
+      trchild = destroy_quadtree_r(qt.topright,x+w/2,y,w/2,centre,radius)
+      blchild = destroy_quadtree_r(qt.bottomleft,x,y+w/2,w/2,centre,radius)
+      brchild = destroy_quadtree_r(qt.bottomright,x+w/2,y+w/2,w/2,centre,radius)
+      if leaf_node(tlchild) and leaf_node(trchild) and leaf_node(blchild) and leaf_node(brchild)
+        sum = tlchild.value+trchild.value+blchild.value+brchild.value
+        if sum == 0 #all black
+          return Quadtree.new(0, nil, nil, nil, nil)
+        elsif sum == 4 # all white
+          return Quadtree.new(1, nil, nil, nil, nil)
+        end
+      end
+      return Quadtree.new(nil, tlchild, trchild, blchild, brchild)
+    else
+      #no overlap
+      return qt
+    end
   end
-  destroy_quadtree_r(qt, 0, 0, 512, 512, centre, radius)
+  destroy_quadtree_r(qt, 0, 0, 512, centre, radius)
 end
 
 #############################
@@ -52,21 +104,21 @@ end
 keyboard = lambda do |key, x, y|
   if key == ?\e || key == ?q
     exit(0)
-  elsif key == ?w
-    $cursor_centre[1] -= 1 if $cursor_centre[1] > 0
-  elsif key == ?s
-    $cursor_centre[1] += 1 if $cursor_centre[1] < 511
-  elsif key == ?a
-    $cursor_centre[0] -= 1 if $cursor_centre[0] > 0
-  elsif key == ?d
-    $cursor_centre[0] += 1 if $cursor_centre[0] < 511
+  # elsif key == ?w
+  #   $cursor_centre[1] -= 1 if $cursor_centre[1] > 0
+  # elsif key == ?s
+  #   $cursor_centre[1] += 1 if $cursor_centre[1] < 511
+  # elsif key == ?a
+  #   $cursor_centre[0] -= 1 if $cursor_centre[0] > 0
+  # elsif key == ?d
+  #   $cursor_centre[0] += 1 if $cursor_centre[0] < 511
   elsif key == ?-
     $cursor_radius -= 1 if $cursor_radius > 1
   elsif key == ?=
     $cursor_radius += 1 if $cursor_radius < 128
   elsif key == " "[0]
     $qmutex.synchronize do
-      destroy_quadtree($quadtree, $cursor_centre, $cursor_radius)
+      $quadtree = destroy_quadtree($quadtree, $cursor_centre, $cursor_radius)
     end
   end
 end
@@ -118,8 +170,23 @@ def display
     glutSwapBuffers
 end
 
+passive_mouse = lambda do |x, y|
+  $cursor_centre = [x,y]
+end
+
+mouse = lambda do |button, state, x, y|
+  if state == GLUT_UP and button == GLUT_LEFT_BUTTON
+    $qmutex.synchronize do
+      $quadtree = destroy_quadtree($quadtree, [x,y], $cursor_radius)
+    end
+  end
+end
+
 glutDisplayFunc(method(:display).to_proc)
 glutTimerFunc(SLEEP_TICKS, method(:timercall).to_proc, 0)
 glutKeyboardFunc(keyboard)
+glutPassiveMotionFunc(passive_mouse)
+glutMotionFunc(passive_mouse)
+glutMouseFunc(mouse)
 glutMainLoop()
 
